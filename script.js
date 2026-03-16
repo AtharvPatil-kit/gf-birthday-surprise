061b57e02c97c9e3675ce158e5ff3a977b0b5dc4
AtharvPatil-kit <atharvapatil808@gmail.com>
2026-03-16 22:17:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 function nextPage(){
 
-window.location.href = "surprise.html";
+window.location.href="surprise.html"
 
 }
 
-setInterval(createHeart,300);
+setInterval(createHeart,400)
 
 function createHeart(){
 
-const heart=document.createElement("div");
+const heart=document.createElement("div")
 
-heart.classList.add("heart");
+heart.classList.add("heart")
 
-heart.innerHTML="💖";
+heart.innerHTML="💖"
 
-heart.style.left=Math.random()*100+"vw";
+heart.style.left=Math.random()*100+"vw"
 
-heart.style.fontSize=(20+Math.random()*20)+"px";
+heart.style.fontSize=(18+Math.random()*15)+"px"
 
-document.body.appendChild(heart);
+document.body.appendChild(heart)
 
 setTimeout(()=>{
-heart.remove();
-},6000);
+
+heart.remove()
+
+},6000)
 
 }
